dddd5d5510914c2d4bb5f781e3f7dd97a168c3c8
Moon Sangdon <96267539+ddodon@users.noreply.github.com>
2024-01-24 13:59:25 +0900

    [Bronze I] Title: 색종이, Time: 112 ms, Memory: 108080 KB, Score: 12 point -BaekjoonHub

---
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -0,0 +1,17 @@
+n = int(input())
+if n<=10:
+    arr = [[0]*101 for _ in range(101)]
+else:
+    arr = [[0]*1001 for _ in range(1001)]
+
+for t in range(1,n+1): #n번 색종이 쌓기
+    # (0,0)을 가장 왼쪽 위 칸으로 가정
+    a,b,c,d = map(int,input().split())
+    for i in range(d):
+        for j in range(c):
+            arr[a+i][b+j] = t
+for t in range(1,n+1):
+    sm = 0 #색종이 넓이
+    for i in range(len(arr)):
+        sm += (arr[i].count(t))
+    print(sm)
