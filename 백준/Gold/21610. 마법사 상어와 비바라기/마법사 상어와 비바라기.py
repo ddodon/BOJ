5c7a654070e3e3f4a6e2bd96f078561f13657fab
Moon Sangdon <96267539+ddodon@users.noreply.github.com>
2024-03-06 17:28:41 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 172 ms, Memory: 112360 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -1,3 +1,5 @@
+# not in 대신 v배열로 구름 유무 검사 (시간 줄이기 실험)
+
 # 8방향
 di = [0,-1,-1,-1,0,1,1,1]
 dj = [-1,-1,0,1,1,1,0,-1]
@@ -10,7 +12,6 @@ def move(lst,d,s):
     d = d-1
     c_move = []
     for ci,cj in lst:
-        v[ci][cj] = False #구름 이동
         ni = (ci+di[d]*s)%(N)
         nj = (cj+dj[d]*s)%(N)
         arr[ni][nj]+=1 #비
@@ -30,21 +31,25 @@ def rain(lst): #구름 위치에 물 복사
 
 N,M= map(int,input().split())
 arr = [list(map(int,input().split())) for _ in range(N)]
-v = [[False]*N for _ in range(N)]
+
 clouds = [(N-2,0),(N-2,1),(N-1,0),(N-1,1)]
-for i, j in clouds:
-    v[i][j] = True
 for _ in range(M):
     # d:방향, s: 거리
     d, s = map(int,input().split())
     n_clouds = move(clouds,d,s) #이동 후 구름 위치
     rain(n_clouds) #물복사
     clouds = []
+
+    v = [[False] * N for _ in range(N)]
+    for ci, cj in n_clouds:
+        v[ci][cj] = True
+
     for i in range(N):
         for j in range(N):
-            if arr[i][j]>=2 and ((i,j) not in n_clouds):
-                arr[i][j] -= 2
-                clouds.append((i,j))
+            if arr[i][j]<2: continue
+            if v[i][j]: continue
+            arr[i][j] -= 2
+            clouds.append((i,j))
 ans = 0
 for i in range(N):
     for j in range(N):
