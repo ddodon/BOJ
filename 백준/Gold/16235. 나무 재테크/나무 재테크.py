04cae7a1608e271663b51533f70034eca63a506b
Moon Sangdon <96267539+ddodon@users.noreply.github.com>
2024-03-08 17:09:56 +0900

    [Gold III] Title: 나무 재테크, Time: 576 ms, Memory: 124884 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.py
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.py
@@ -1,74 +1,69 @@
-# 8방향
-di = [-1, -1, 0, 1, 1, 1, 0, -1]
+# dic -> 단순 for문 + 함수 리턴값/리스트 제거 실험
+# set (1200ms)
+# dic (시간초과)
+di = [-1, -1, 0, 1, 1, 1, 0, -1]                    # 8방향
 dj = [0, 1, 1, 1, 0, -1, -1, -1]
 
-def spring(loc):
-    dead = []
+def spring():
+    for i in range(N):
+        for j in range(N):
+            if tree[i][j]:
+                tree[i][j].sort()
+                l = len(tree[i][j])
+                for z in range(l):
+                    if feed[i][j] < tree[i][j][z]:
+                        for t in tree[i][j][z:l]:
+                            newtree[i][j] += t // 2
+                        tree[i][j] = tree[i][j][:z]
+                        break
+                    else:
+                        feed[i][j] -= tree[i][j][z]
+                        tree[i][j][z] += 1
 
-    for i,j in loc:
-        tlst = tree[i][j]
-        tlst.sort()
-        new_tree = []
-        for t in range(len(tlst)):
-            if feed[i][j] < tlst[t]:
-                dead.append((i,j,tlst[t]))
-            else:
-                feed[i][j] -= tlst[t]
-                tlst[t] += 1
-                new_tree.append(tlst[t])
-        tree[i][j] = new_tree
-    return dead
+def summer():
+    for i in range(N):
+        for j in range(N):
+            feed[i][j] += newtree[i][j]
 
-def summer(dead):
-    for d in range(len(dead)):
-        i,j,f = dead[d]
-        feed[i][j] += f//2
 
-def autumn(loc):
-    n_loc = set()
-    if loc:
-        for (i, j) in loc:
-            for t in tree[i][j]:
-                if t != 0 and t % 5 == 0:
-                    for k in range(len(di)):
-                        ni = i + di[k]
-                        nj = j + dj[k]
-                        if 0 <= ni < N and 0 <= nj < N:
-                            tree[ni][nj].append(1)
-                            n_loc.add((ni, nj))
-    for (i,j) in n_loc:
-        loc.add((i,j))
+def autumn():
+    for i in range(N):
+        for j in range(N):
+            if tree[i][j]:
+                for z in tree[i][j]:
+                    if z%5==0:
+                        for k in range(len(di)):
+                            ni = i + di[k]
+                            nj = j + dj[k]
+                            if 0 <= ni < N and 0 <= nj < N:
+                                tree[ni][nj].append(1)
 
 def winter():
     for i in range(N):
         for j in range(N):
             feed[i][j] += robot[i][j]
 
-N, M, K = map(int, input().split())
+def cal():
+    ans = 0
+    for i in range(N):
+        for j in range(N):
+           ans += len(tree[i][j])
+    return ans
+
+N, M, K = map(int, input().split())             # 입력
 robot = [list(map(int, input().split())) for _ in range(N)]
 feed = [[5 for _ in range(N)] for _ in range(N)]
-loc = set()
-# dic = defaultdict(int)
 tree = [[[] for _ in range(N)] for _ in range(N)]
-
-# 나무 행/ 열 / 나무 나이
 for _ in range(M):
     x, y, z = map(int, input().split())
     x, y = x - 1, y - 1
-    loc.add((x,y))
     tree[x][y].append(z)
-for _ in range(K):
-    # 봄 / 여름 / 가을 / 겨울
-    dead = spring(loc)
-    summer(dead)
-    autumn(loc)
+
+for _ in range(K):                              # 4계절
+    newtree = [[0] * N for _ in range(N)]
+    spring()
+    summer()
+    autumn()
     winter()
-
-# for lst in tree:
-#     print(lst)
-
-ans = 0
-for i in range(N):
-    for j in range(N):
-        ans += len(tree[i][j])
-print(ans)
+    
+print(cal())                                    # 나무 개수
