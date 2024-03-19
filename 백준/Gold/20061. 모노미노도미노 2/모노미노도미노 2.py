1670c47763f0a22bf150006cddea06585a88f117
Moon Sangdon <96267539+ddodon@users.noreply.github.com>
2024-03-19 21:20:47 +0900

    [Gold II] Title: 모노미노도미노 2, Time: 232 ms, Memory: 112308 KB -BaekjoonHub

--- 백준/Gold/20061. 모노미노도미노 2/모노미노도미노 2.py
+++ 백준/Gold/20061. 모노미노도미노 2/모노미노도미노 2.py
@@ -1,180 +1,74 @@
-# 총 시간: 분
-# 문제 읽기:  ~
-# 풀이 구상:  ~
-# 로직 구현:  ~ 2:07:00
-# 오류 수정:  2:12:00 ~
-# 제출 결과: (/3)
-# 오류 부분: 정답 제출 위한 코드에서 방향 좌표 빼먹음 + 한줄 완성 시 터지고 움직일 때 처음부터 움직이게 설계함
-# 1시간 30분 경 잠깐 휴식
-# 틀렸습니다(1%)
 
-di = [-1,0,1,0]
-dj = [0,1,0,-1]
+'''
+Memo Boj 20061 모노미노도미노2
+메모리: 119184 KB
+시간: 612 ms
+결과: (1/3)
+풀이시간: 분 
+▷ 문제 읽기: 분
+▷ 풀이 구상: 분
+▷ 로직 구현: 분
+▷ 오류 수정: @분
+▷ 오류 부분: 정답 제출 위한 코드에서 방향 좌표 빼먹음 + 한줄 완성 시 터지고 움직일 때 처음부터 움직이게 설계함
+    (풀이 시작 90분 / 잠깐 휴식)
+리팩토링 버전
+'''
+
+def gravity(arr, t, line):  # line: 행 or 열
+    if t == 1:              # 1x1 도미노 중력
+        for i in range(B):
+            if arr[i][line] == 1:
+                arr[i - 1][line] = 1
+                break
+        else:
+            arr[B - 1][line] = 1
+
+    elif t == 2:            # 1x2 도미노 중력
+        for i in range(B):
+            if arr[i][line] == 1 or arr[i][line + 1] == 1:
+                arr[i - 1][line] = arr[i - 1][line + 1] = 1
+                break
+        else:
+            arr[B - 1][line] = arr[B - 1][line + 1] = 1
+
+    elif t == 3:            # 2x1 도미노 중력
+        for i in range(B):
+            if arr[i][line] == 1:
+                arr[i - 1][line] = arr[i - 2][line] = 1
+                break
+        else:
+            arr[B - 1][line] = arr[B - 2][line] = 1
+    return arr
+
+def bubble(arr):
+    global ans
+    for i in range(B):      # 한 줄 완성 시 버블
+        if arr[i].count(1) >= 4:
+            arr.pop(i)
+            arr.insert(0,[0]*A)
+            ans += 1
+    for _ in range(2):      # 연한 칸에 도미노 있으면 버블
+        if arr[1].count(1) >= 1:
+            arr.pop(B-1)
+            arr.insert(0, [0] * A)
+    return arr
 
 N = int(input())
-A,B = 4,10
-
-def to_blue(t,x,y):
-    d = 1
-    start = [(x,y)]
-    if t==2: start.append((x,y+1))
-    elif t==3: start.append((x+1,y))
-    flag = True
-    while flag:
-        for n,(ci,cj) in enumerate(start):
-            ni = ci+di[d]
-            nj = cj+dj[d]
-            if not(0<=ni<A and 0<=nj<B and blue[ni][nj]==0):
-                flag = False
-                num = n
-                point=(ci,cj)
-                break
-            start[n] = (ni,nj)
-    if num==0:
-        for si,sj in start:
-            blue[si][sj] = 1
-    else:
-        ei,ej = point
-        if t==2:
-            blue[ei][ej-1]=blue[ei][ej]=1
-        else:
-            blue[ei-1][ej]=blue[ei][ej] = 1
-    return blue
-
-
-def to_green(t,x,y):
-    d = 2
-    start = [(x,y)]
-    if t==2: start.append((x,y+1))
-    elif t==3: start.append((x+1,y))
-    flag = True
-    while flag:
-        for n,(ci,cj) in enumerate(start):
-            ni = ci+di[d]
-            nj = cj+dj[d]
-            if not(0<=ni<B and 0<=nj<A and green[ni][nj]==0):
-                flag = False
-                num = n
-                point=(ci,cj)
-                break
-            start[n] = (ni,nj)
-    if num==0:
-        for si,sj in start:
-            green[si][sj] = 1
-    else:
-        ei,ej = point
-        if t==2:
-            green[ei][ej-1]=green[ei][ej]=1
-        else:
-            green[ei-1][ej]=green[ei][ej] = 1
-    return green
-
-
-def check_green(arr):
-    d = 2
-    st = []
-    cnt = 0 # 사라진 행의 수
-    for i in range(B-1,A+1,-1):
-        if arr[i].count(1)==4:
-            cnt+=1
-            arr[i] = [0]*A
-            st.append(i)
-    if st:
-        for i in range(st[0],A-1,-1):
-            for j in range(A-1,-1,-1):
-                if arr[i][j]!=1: continue
-                ci,cj = i, j
-                for t in range(cnt):
-                    ni,nj = ci+di[d],cj+dj[d]
-                    if 0<=ni<B and arr[ni][nj]==0:
-                        arr[ni][nj] = 1
-                        arr[ci][cj] = 0
-                        ci,cj = ni,nj
-    return arr,cnt
-
-def check_blue(arr):
-    d = 2
-    cnt = 0 # 사라진 열의 수
-    st = []
-    arr = [list(lst) for lst in zip(*arr[::-1])]
-    for i in range(B-1,A+1,-1):
-        if arr[i].count(1)==4:
-            cnt+=1
-            arr[i] = [0]*A
-            st.append(i)
-    if st:
-        for i in range(st[0],A-1,-1):
-            for j in range(A-1,-1,-1):
-                if arr[i][j]!=1: continue
-                ci,cj = i, j
-                for t in range(cnt):
-                    ni,nj = ci+di[d],cj+dj[d]
-                    if 0<=ni<B and arr[ni][nj]==0:
-                        arr[ni][nj] = 1
-                        arr[ci][cj] = 0
-                        ci,cj = ni,nj
-    arr = [list(lst) for lst in zip(*arr)][::-1]
-    return arr,cnt
-
-def soft_blue(arr):
-    d = 2
-    cnt = 0
-    arr = [list(lst) for lst in zip(*arr[::-1])]
-    for i in range(A,A+2):
-        if arr[i].count(1)>0:
-            cnt+=1
-    for j in range(cnt):
-        arr[B-1-j] = [0]*A
-    for i in range(B-1,A-1,-1):
-        for j in range(A-1,-1,-1):
-            if arr[i][j]!=1: continue
-            ci,cj = i, j
-            for t in range(cnt):
-                ni,nj = ci+di[d],cj+dj[d]
-                if 0<=ni<B and arr[ni][nj]==0:
-                    arr[ni][nj] = 1
-                    arr[ci][cj] = 0
-                    ci,cj = ni,nj
-    arr = [list(lst) for lst in zip(*arr)][::-1]
-    return arr
-
-
-def soft_green(arr):
-    d = 2
-    cnt = 0
-    for i in range(A,A+2):
-        if arr[i].count(1)>0:
-            cnt+=1
-    for j in range(cnt):
-        arr[B-1-j] = [0]*A
-    for i in range(B-1,A-1,-1):
-        for j in range(A-1,-1,-1):
-            if arr[i][j]!=1: continue
-            ci,cj = i, j
-            for t in range(cnt):
-                ni,nj = ci+di[d],cj+dj[d]
-                if 0<=ni<B and arr[ni][nj]==0:
-                    arr[ni][nj] = 1
-                    arr[ci][cj] = 0
-                    ci,cj = ni,nj
-    return arr
-
-
-blue = [[0]*(B) for _ in range(A)]
-green = [[0]*A for _ in range(B)]
+A, B = 4, 6
+green = [[0] * A for _ in range(B)]
+blue = [[0] * A for _ in range(B)]
+blue_table = {1: 1, 2: 3, 3: 2}  # 파란색은 2-3번 도미노가 반대
 ans = 0
 for _ in range(N):
-    t,x,y = map(int,input().split())
-    blue = to_blue(t,x,y)
-    green = to_green(t,x,y)
+    t, x, y = map(int, input().split())
 
-    green,g_cnt = check_green(green)
-    ans += g_cnt
-    blue,b_cnt = check_blue(blue)
-    ans += b_cnt
-    blue = soft_blue(blue)
-    green = soft_green(green)
-print(ans)
-sm_blue = sum(map(sum,blue))
-sm_green = sum(map(sum,green))
-print(sm_blue+sm_green)
+    green = gravity(green, t, y)
+    blue = gravity(blue, blue_table[t], x)
+
+    green = bubble(green)
+    blue = bubble(blue)
+
+print(ans)  # 총 점수
+sm_blue = sum(map(sum, blue))
+sm_green = sum(map(sum, green))
+print(sm_blue + sm_green)  # 총 블럭 개수
