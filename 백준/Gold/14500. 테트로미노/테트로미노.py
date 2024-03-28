86d38be22ca78c7e983c10c390931f7b0c1cb130
Moon Sangdon <96267539+ddodon@users.noreply.github.com>
2024-03-29 08:46:32 +0900

    [Gold IV] Title: 테트로미노, Time: 188 ms, Memory: 114652 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,82 +1,32 @@
-def case_1(i, j):  # 2x2정사각형
-    try: return (arr[i][j] + arr[i][j + 1] + arr[i + 1][j] + arr[i + 1][j + 1])
-    except: return 0
-def case_2(i, j):  # 1x4 줄
-    try: return (arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i][j + 3])
-    except: return 0
-def case_2T(i, j):  # 4x1 줄
-    try: return (arr[i][j] + arr[i + 1][j] + arr[i + 2][j] + arr[i + 3][j])
-    except: return 0
-case1_2 = [case_1, case_2, case_2T]
+di = [-1, 0, 1, 0]
+dj = [0, 1, 0, -1]
 
-def case_3a(i,j):
-    try: return (arr[i][j] + arr[i+1][j] + arr[i+2][j] + arr[i+2][j+1])
-    except: return 0
-def case_3b(i,j):
-    try: return (arr[i+1][j] + arr[i+1][j+1] + arr[i+1][j+2] + arr[i][j+2])
-    except: return 0
-def case_3c(i,j):
-    try: return (arr[i][j] + arr[i][j+1] + arr[i+1][j+1] + arr[i+2][j+1])
-    except: return 0
-def case_3d(i,j):
-    try: return (arr[i][j] + arr[i+1][j] + arr[i][j+1] + arr[i][j+2])
-    except: return 0
 
-def case_3aT(i,j):
-    try: return (arr[i][j+1] + arr[i+1][j+1] + arr[i+2][j+1] + arr[i+2][j])
-    except: return 0
-def case_3bT(i,j):
-    try: return (arr[i][j] + arr[i+1][j] + arr[i+1][j+1] + arr[i+1][j+2])
-    except: return 0
-def case_3cT(i,j):
-    try: return (arr[i][j] + arr[i][j+1] + arr[i+1][j] + arr[i+2][j])
-    except: return 0
-def case_3dT(i,j):
-    try: return (arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+2])
-    except: return 0
-case3 = [case_3a, case_3aT, case_3b, case_3bT, case_3c, case_3cT, case_3d, case_3dT]
+def dfs(n, sm, tlst):
+    global ans
+    if ans >= sm + (4 - n) * mx:
+        return
+    if n == 4:
+        ans = max(sm, ans)
+        return
 
-def case_4a(i,j):
-    try: return (arr[i][j] + arr[i+1][j] + arr[i+1][j+1] + arr[i+2][j+1])
-    except: return 0
-def case_4b(i,j):
-    try: return (arr[i+1][j] + arr[i+1][j+1] + arr[i][j+1] + arr[i][j+2])
-    except: return 0
-def case_4c(i,j):
-    try: return (arr[i+2][j] + arr[i+1][j] + arr[i+1][j+1] + arr[i][j+1])
-    except: return 0
-def case_4d(i,j):
-    try: return (arr[i][j] + arr[i][j+1] + arr[i+1][j+1] + arr[i+1][j+2])
-    except: return 0
-case4 = [case_4a, case_4b, case_4c, case_4d]
-
-def case_5a(i,j):
-    try: return (arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1])
-    except: return 0
-def case_5b(i,j):
-    try: return (arr[i][j] + arr[i+1][j] + arr[i+1][j+1] + arr[i+2][j])
-    except: return 0
-def case_5c(i,j):
-    try: return (arr[i+1][j] + arr[i+1][j+1] + arr[i][j+1] + arr[i+1][j+2])
-    except: return 0
-def case_5d(i,j):
-    try: return (arr[i+1][j] + arr[i][j+1] + arr[i+1][j+1] + arr[i+2][j+1])
-    except: return 0
-case5 = [case_5a, case_5b,  case_5c, case_5d]
-
+    for ci, cj in tlst:
+        for k in range(len(di)):
+            ni = ci + di[k]
+            nj = cj + dj[k]
+            if 0 <= ni < N and 0 <= nj < M and v[ni][nj] == 0:
+                v[ni][nj] = 1
+                dfs(n + 1, sm + arr[ni][nj], tlst + [(ni, nj)])
+                v[ni][nj] = 0
 
 
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-mx = 0
+v = [[0] * M for _ in range(N)]
+mx = max(map(max, arr))
+ans = 0
 for i in range(N):
     for j in range(M):
-        for c in case1_2:
-            mx = max(mx, c(i, j))
-        for c in case3:
-            mx = max(mx, c(i, j))
-        for c in case4:
-            mx = max(mx, c(i, j))
-        for c in case5:
-            mx = max(mx, c(i, j))
-print(mx)
+        v[i][j] = 1
+        dfs(1, arr[i][j], [(i, j)])
+print(ans)
