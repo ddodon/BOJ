685a928a4370efa294e5732b53d252df4f6e1799
Moon Sangdon <96267539+ddodon@users.noreply.github.com>
2024-04-02 12:49:37 +0900

    [Gold III] Title: 연구소 3, Time: 236 ms, Memory: 115012 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.py
+++ 백준/Gold/17142. 연구소 3/연구소 3.py
@@ -3,58 +3,63 @@ from collections import deque
 di = [-1, 0, 1, 0]
 dj = [0, 1, 0, -1]
 
-def dfs(n, idx, tlst): # M개의 활성 바이러스 조합
+
+def dfs(n, idx, tlst):
     global ans
+    # M개 바이러스 선택
     if n == M:
-        ans = min(ans, bfs(tlst))
+        res = spread(tlst)
+        ans = min(ans, res)
         return
 
     for j in range(idx, len(virus)):
-        if v[j] == 0:
-            v[j] = 1
-            dfs(n + 1, j+1, tlst + [virus[j]])
-            v[j] = 0
+        if virus_v[j] == 0:
+            virus_v[j] = 1
+            dfs(n + 1, j, tlst + virus[j])
+            virus_v[j] = 0
 
-def bfs(act): # 조합별 최소 시간 구하기
+
+def spread(active_viruses):
     q = deque()
-    bfs_v = [[0] * N for _ in range(N)]
-    for (ai, aj) in act:
-        bfs_v[ai][aj] = 1
-        q.append((ai, aj, 1))
-    remain = blank
+    v = [[0] * N for _ in range(N)]
+    for vi, vj in active_viruses:
+        q.append((vi, vj))
+        v[vi][vj] = 1
+    cnt = 0
     while q:
-        ci, cj, dist = q.popleft()
+        ci, cj = q.popleft()
         for k in range(len(di)):
             ni = ci + di[k]
             nj = cj + dj[k]
-            if 0 <= ni < N and 0 <= nj < N and bfs_v[ni][nj] == 0 and arr[ni][nj] != 1:
-                if arr[ni][nj] == 0:
-                    remain -= 1
-                    bfs_v[ni][nj] = dist + 1
-                    q.append((ni, nj, dist + 1))
-                    if remain == 0:
-                        return (max(map(max, bfs_v)) - 1)
-                elif arr[ni][nj] == 2:
-                    bfs_v[ni][nj] = dist
-                    q.append((ni, nj, dist + 1))
-    return 21e8
+            if 0<=ni<N and 0<=nj<N and v[ni][nj]==0 and arr[ni][nj]!=1:
+                # 비활성 바이러스
+                if arr[ni][nj]==2:
+                    v[ni][nj] = v[ci][cj]+1
+                else:
+                    v[ni][nj] = v[ci][cj]+1
+                    cnt += 1
+                    if blank == cnt:
+                        return (max(map(max, v))) - 1
+                q.append((ni,nj))
 
+
+    else:
+        return 21e8
 
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-ans = 21e8
-blank = 0
-
 virus = []
+blank = 0 #남은 빈칸 개수
+ans = 21e8
 for i in range(N):
     for j in range(N):
-        if arr[i][j] == 0:
+        if arr[i][j] == 2:
+            virus.append([(i, j)])
+        elif arr[i][j] == 0:
             blank += 1
-        elif arr[i][j] == 2:
-            virus.append((i, j)) # 빈칸과 바이러스의 좌표 담아두기
-v = [0] * (len(virus) + 1)
+virus_v = [0] * len(virus)
+dfs(0, 0, [])
 if blank == 0:
     print(0)
-    exit()
-dfs(0, 0, [])
-print(-1 if ans == 21e8 else ans)
+else:
+    print(-1 if ans==21e8 else ans)
