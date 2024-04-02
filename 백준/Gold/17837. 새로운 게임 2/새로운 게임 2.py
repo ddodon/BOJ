a0d6c4ccd2c57765abfcf1df595faade0217a318
Moon Sangdon <96267539+ddodon@users.noreply.github.com>
2024-04-02 15:20:51 +0900

    [Gold II] Title: 새로운 게임 2, Time: 200 ms, Memory: 112888 KB -BaekjoonHub

--- 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.py
+++ 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.py
@@ -1,82 +1,72 @@
-# 총 시간: 분
-# 문제 읽기: 0:14:00 ~ 0:23:00
-# 풀이 구상:
-# 로직 구현:
-# 오류 수정:  ~ 2:02:40
-# 제출 결과: (/2)
-# 오류 부분: 말 변수의 좌표가 고정되어 있었음, 말이 한꺼번에 이동할 때 말 좌표 안바뀜,indexerror, chess배열 생성시 K개 만큼 만들었따...
 
 di = [0, 0, -1, 1]
 dj = [1, -1, 0, 0]
+opp = {0: 1, 1: 0, 2: 3, 3: 2}
+White,Red,Blue = 0,1,2
 
+def move():
+    # 1번말부터
+    for horse in range(1,K+1):
+        ci,cj,cd = horse_info[horse]
+        horse_idx = arr[ci][cj].index(horse)
+        # 1. 이동 (다음 칸 확인)
+        ni = ci + di[cd]
+        nj = cj + dj[cd]
+        # 1-1. 다음 칸이 범위 밖 or 파란칸이면 반대 이동
+        if not (0<=ni<N and 0<=nj<N) or color[ni][nj] == Blue:
+            cd = opp[cd]
+            ni = ci + di[cd]
+            nj = cj + dj[cd]
+            if not (0 <= ni < N and 0 <= nj < N) or color[ni][nj] == Blue:
+                horse_info[horse] = [ci, cj, cd]
+                continue
+            elif color[ni][nj] == White:
+                for h in range(horse_idx, len(arr[ci][cj])):
+                    arr[ni][nj].append(arr[ci][cj][h])
+                    if arr[ci][cj][h]==horse:
+                        horse_info[arr[ci][cj][h]][2] = cd
+                    horse_info[arr[ci][cj][h]][0],horse_info[arr[ci][cj][h]][1] = ni,nj
+            else:
+                for h in range(len(arr[ci][cj])-1,horse_idx-1, -1):
+                    arr[ni][nj].append(arr[ci][cj][h])
+                    if arr[ci][cj][h]==horse:
+                        horse_info[arr[ci][cj][h]][2] = cd
+                    horse_info[arr[ci][cj][h]][0],horse_info[arr[ci][cj][h]][1] = ni,nj
 
-def move(ci, cj, d, horse):
-    idx = chess[ci][cj].index(horse)
-    ni = ci + di[d]
-    nj = cj + dj[d]
-    if 0 <= ni < N and 0 <= nj < N and color[ni][nj] != Blue:
-        if color[ni][nj] == White:
-            chess[ni][nj] += chess[ci][cj][idx:]
-            chess[ci][cj] = chess[ci][cj][:idx]
-        elif color[ni][nj] == Red:
-            chess[ni][nj] += chess[ci][cj][idx:][::-1]
-            chess[ci][cj] = chess[ci][cj][:idx]
-    else:
-        if d == 0:
-            d = 1
-        elif d == 1:
-            d = 0
-        elif d == 2:
-            d = 3
-        elif d == 3:
-            d = 2
-        ni = ci + di[d]
-        nj = cj + dj[d]
-        if (0 <= ni < N and 0 <= nj < N) and color[ni][nj] != Blue:
-            if color[ni][nj] == White:
-                chess[ni][nj] += chess[ci][cj][idx:]
-                chess[ci][cj] = chess[ci][cj][:idx]
-            elif color[ni][nj] == Red:
-                chess[ni][nj] += chess[ci][cj][idx:][::-1]
-                chess[ci][cj] = chess[ci][cj][:idx]
+        # 1-2. 흰색 칸이면
+        elif color[ni][nj] == White:
+            for h in range(horse_idx, len(arr[ci][cj])):
+                arr[ni][nj].append(arr[ci][cj][h])
+                horse_info[arr[ci][cj][h]][0],horse_info[arr[ci][cj][h]][1] = ni,nj
+
+        # 1-3. 빨간 칸이면
         else:
-            ni, nj = ci, cj
-    return (ni, nj, d)
+            for h in range(len(arr[ci][cj]) - 1, horse_idx - 1, -1):
+                arr[ni][nj].append(arr[ci][cj][h])
+                horse_info[arr[ci][cj][h]][0],horse_info[arr[ci][cj][h]][1] = ni,nj
+        arr[ci][cj] = arr[ci][cj][:horse_idx]
 
-
-def check():
-    for i in range(N):
-        for j in range(N):
-            if len(chess[i][j]) >= 4:
-                return True
+        # 2. 말 4개 쌓였는지 검사
+        if arr[ci][cj] and len(arr[ci][cj]) > 3:
+            return True
+        elif arr[ni][nj] and len(arr[ni][nj]) > 3:
+            return True
     return False
 
-
-White, Red, Blue = 0, 1, 2
 N, K = map(int, input().split())
 color = [list(map(int, input().split())) for _ in range(N)]
-chess = [[[] for _ in range(N)] for _ in range(N)]
-order = []
+arr = [[[] for _ in range(N)] for _ in range(N)]
+horse_info = [[0]]
 for k in range(1, K + 1):
-    r, c, d = map(int, input().split())
-    chess[r - 1][c - 1].append(k)
-    order.append([r - 1, c - 1, d - 1])
-ans = 0
-
-flag = True
-while flag:
-    ans += 1
-    if ans > 1000:
+    x, y, d = map(int, input().split())
+    x, y, d = x - 1, y - 1, d - 1
+    horse_info.append([x,y,d])
+    arr[x][y].append(k)
+for t in range(1, 1001):
+    # 1. 말의 이동 (순서대로)
+    flag = move()
+    if flag:
+        print(t)
         break
-    for i in range(K):
-        cr, cc, cd = order[i][0], order[i][1], order[i][2]
-        (nr, nc, nd) = move(order[i][0], order[i][1], order[i][2], i + 1)
-        order[i][0], order[i][1], order[i][2] = nr, nc, nd
-        for h in chess[nr][nc]:
-            if h == 0: continue
-            order[h - 1][0], order[h - 1][1] = nr, nc
-        res = check()
-        if res:
-            flag = False
-            break
-print(-1 if ans > 1000 else ans)
+else:
+    print(-1)
