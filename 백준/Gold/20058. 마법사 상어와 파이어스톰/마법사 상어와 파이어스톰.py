df6814249547bfbcbb90438702b614d65616f28d
Moon Sangdon <96267539+ddodon@users.noreply.github.com>
2024-04-03 12:43:15 +0900

    [Gold III] Title: 마법사 상어와 파이어스톰, Time: 592 ms, Memory: 125640 KB -BaekjoonHub

--- 백준/Gold/20058. 마법사 상어와 파이어스톰/마법사 상어와 파이어스톰.py
+++ 백준/Gold/20058. 마법사 상어와 파이어스톰/마법사 상어와 파이어스톰.py
@@ -1,18 +1,5 @@
-# 좌표기준 for문 전치행렬
 from collections import deque
 
-di = [-1, 0, 1, 0]
-dj = [0, 1, 0, -1]
-
-def check(i, j):
-    cnt = 0
-    for k in range(len(di)):
-        ni = i + di[k]
-        nj = j + dj[k]
-        if 0 <= ni < 2 ** N and 0 <= nj < 2 ** N and arr[ni][nj] > 0:
-            cnt += 1
-    if cnt < 3:
-        return (i, j)
 
 def bfs(si, sj):
     q = deque()
@@ -24,45 +11,59 @@ def bfs(si, sj):
         for k in range(len(di)):
             ni = ci + di[k]
             nj = cj + dj[k]
-            if 0 <= ni < 2 ** N and 0 <= nj < 2 ** N and arr[ni][nj] > 0 and v[ni][nj] == 0:
-                cnt += 1
+            if 0 <= ni < 2 ** N and 0 <= nj < 2 ** N and v[ni][nj] == 0 and arr[ni][nj] > 0:
                 q.append((ni, nj))
                 v[ni][nj] = 1
+                cnt += 1
     return cnt
 
-N, Q = map(int, input().split())
-arr_b = [list(map(int, input().split())) for _ in range(2 ** N)]
 
-v = [[0] * (2 ** N) for _ in range(2 ** N)]
-ans1 = 0
-ans2 = 0
-qq = list(map(int, input().split()))
+di = [-1, 0, 1, 0]
+dj = [0, 1, 0, -1]
 
-for q in qq:
-    q = 2 ** q
-    arr = [[0] * (2 ** N) for _ in range(2 ** N)]
-    for si in range(0, 2 ** N, q):
-        for sj in range(0, 2 ** N, q):
-            for i in range(q):
-                for j in range(q):
-                    arr[si+i][sj+j] = arr_b[si+q-1-j][sj+i]
 
-    non_ice = []
+def melt(arr):
+    nrr = [lst[::] for lst in arr]
     for i in range(2 ** N):
         for j in range(2 ** N):
-            non_ice.append(check(i, j))
-    for loc in non_ice:
-        if loc:
-            ci, cj = loc
-            arr[ci][cj] -= 1
-    arr_b = arr
+            cnt = 0
+            if arr[i][j] <= 0: continue
+            ci, cj = i, j
+            for k in range(len(di)):
+                ni = ci + di[k]
+                nj = cj + dj[k]
+                if not (0 <= ni < 2 ** N and 0 <= nj < 2 ** N) or arr[ni][nj] == 0:
+                    continue
+                else:
+                    cnt += 1
+            if cnt < 3:
+                nrr[ci][cj] -= 1
+    return nrr
 
+
+N, Q = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(2 ** N)]
+order = list(map(int, input().split()))  # 마법 횟수
+
+for q in range(Q):
+    L = 2 ** order[q]
+    # 1. 부분 회전
+    nrr = [lst[::] for lst in arr]
+    for si in range(0, 2 ** N, L):
+        for sj in range(0, 2 ** N, L):
+            for i in range(L):
+                for j in range(L):
+                    arr[si + i][sj + j] = nrr[si + L - 1 - j][sj + i]
+
+    # 2. 얼음 녹이기
+    arr = melt(arr)
+
+v = [[0] * (2 ** N) for _ in range(2 ** N)]
+mx = 0
 for i in range(2 ** N):
     for j in range(2 ** N):
         if arr[i][j] > 0 and v[i][j] == 0:
-            ans2 = max(ans2, bfs(i, j))
-        if arr[i][j] > 0:
-            ans1 += arr[i][j]
-
-print(ans1)
-print(ans2)
+            res = bfs(i, j)
+            mx = max(res, mx)
+print(sum(map(sum, arr)))
+print(mx)
