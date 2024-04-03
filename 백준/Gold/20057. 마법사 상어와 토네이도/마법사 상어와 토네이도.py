0bd906b5c95df36dfa6f40e7bdb21db9c007e713
Moon Sangdon <96267539+ddodon@users.noreply.github.com>
2024-04-03 11:25:27 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 280 ms, Memory: 113656 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
@@ -1,93 +1,31 @@
+# 시간복잡도 비교 (tornado 함수 호출 시)
 di = [0, 1, 0, -1]
 dj = [-1, 0, 1, 0]
-
-ti = [-2, -1, -1, -1, 0, 1, 1, 1, 2]
-tj = [0, -1, 0, 1, -2, -1, 0, 1, 0]
-
-ratio = [0.02, 0.1, 0.07, 0.01, 0.05, 0.1, 0.07, 0.01, 0.02]
+ai = [[-2, -1, -1, -1, 0, 1, 1, 1, 2, 0],
+      [0, 1, 0, -1, 2, 1, 0, -1, 0, 1],
+      [2, 1, 1, 1, 0, -1, -1, -1, -2, 0],
+      [0, -1, 0, 1, -2, -1, 0, 1, 0, -1]]
+aj = [[0, -1, 0, 1, -2, -1, 0, 1, 0, -1],
+      [-2, -1, -1, -1, 0, 1, 1, 1, 2, 0],
+      [0, 1, 0, -1, 2, 1, 0, -1, 0, 1],
+      [2, 1, 1, 1, 0, -1, -1, -1, -2, 0]]
+ratio = [0.02, 0.1, 0.07, 0.01, 0.05, 0.1, 0.07, 0.01, 0.02, 0]
 
 def tornado(ci, cj, d):
     global ans
     s = arr[ci][cj]
-    if d == 0:
-        for k in range(len(ti)):
-            ni = ci + ti[k]
-            nj = cj + tj[k]
-            sand = int(s * (ratio[k]))
-            if 0 <= ni < N and 0 <= nj < N:
-                arr[ni][nj] += sand
-                arr[ci][cj] -= sand
-            else:
-                ans += sand
-                arr[ci][cj] -= sand
-        ni = ci + 0
-        nj = cj + (-1)
+    arr[ci][cj]=0
+    sm = 0
+    for loc in range(10):
+        ni, nj = ci + ai[d][loc], cj + aj[d][loc]
+        t = int(s * ratio[loc])
+        if loc == 9:
+            t = s-sm
         if 0 <= ni < N and 0 <= nj < N:
-            arr[ni][nj] += arr[ci][cj]
-            arr[ci][cj] = 0
+            arr[ni][nj] += t
         else:
-            ans += arr[ci][cj]
-            arr[ci][cj] = 0
-
-    elif d == 1:
-        for k in range(len(ti)):
-            ni = ci - tj[k]
-            nj = cj + ti[k]
-            sand = int(s * (ratio[k]))
-            if 0 <= ni < N and 0 <= nj < N:
-                arr[ni][nj] += sand
-                arr[ci][cj] -= sand
-            else:
-                ans += sand
-                arr[ci][cj] -= sand
-        ni = ci + 1
-        nj = cj + 0
-        if 0 <= ni < N and 0 <= nj < N:
-            arr[ni][nj] += arr[ci][cj]
-            arr[ci][cj] = 0
-        else:
-            ans += arr[ci][cj]
-            arr[ci][cj] = 0
-
-    elif d == 2:
-        for k in range(len(ti)):
-            ni = ci + ti[k]
-            nj = cj - tj[k]
-            sand = int(s * (ratio[k]))
-            if 0 <= ni < N and 0 <= nj < N:
-                arr[ni][nj] += sand
-                arr[ci][cj] -= sand
-            else:
-                ans += sand
-                arr[ci][cj] -= sand
-        ni = ci + 0
-        nj = cj + 1
-        if 0 <= ni < N and 0 <= nj < N:
-            arr[ni][nj] += arr[ci][cj]
-            arr[ci][cj] = 0
-        else:
-            ans += arr[ci][cj]
-            arr[ci][cj] = 0
-
-    elif d == 3:
-        for k in range(len(ti)):
-            ni = ci + tj[k]
-            nj = cj - ti[k]
-            sand = int(s * (ratio[k]))
-            if 0 <= ni < N and 0 <= nj < N:
-                arr[ni][nj] += sand
-                arr[ci][cj] -= sand
-            else:
-                ans += sand
-                arr[ci][cj] -= sand
-        ni = ci - 1
-        nj = cj + 0
-        if 0 <= ni < N and 0 <= nj < N:
-            arr[ni][nj] += arr[ci][cj]
-            arr[ci][cj] = 0
-        else:
-            ans += arr[ci][cj]
-            arr[ci][cj] = 0
+            ans += t
+        sm += t
 
 
 N = int(input())
@@ -98,16 +36,15 @@ cnt = 0  # 지나온 칸
 hold = 1  # 지나가는 칸
 flag = 0  # 지나가는 칸 개수 늘리기
 ans = 0  # 격자 밖으로 나간 모래
+# 달팽이
 while 1:
     if (ci, cj) == (0, 0):
         break
     ni = ci + di[d]
     nj = cj + dj[d]
     tornado(ni, nj, d)
-
     cnt += 1
     ci, cj = ni, nj
-    # 방향이동
     if cnt == hold:
         flag += 1
         if flag == 2:
