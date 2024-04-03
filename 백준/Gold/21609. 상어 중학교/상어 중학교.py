d18664159d6e329cd9c43e3f6f247073cd92bd89
Moon Sangdon <96267539+ddodon@users.noreply.github.com>
2024-04-03 14:21:43 +0900

    [Gold II] Title: 상어 중학교, Time: 232 ms, Memory: 115632 KB -BaekjoonHub

--- 백준/Gold/21609. 상어 중학교/상어 중학교.py
+++ 백준/Gold/21609. 상어 중학교/상어 중학교.py
@@ -1,108 +1,85 @@
-# 총 시간: 분
-# 문제 읽기:  ~
-# 풀이 구상:  ~
-# 로직 구현: 10:09 ~
-# 오류 수정:  ~
-# 제출 결과: (/3)
-# 오류 부분: 전부 0인경우, 무지개블럭은 매번 포함했어야 함
-
-from collections import defaultdict, deque
+from collections import deque
 
 di = [-1, 0, 1, 0]
 dj = [0, 1, 0, -1]
 
-def bfs(i, j, block, num):
+
+def bfs(si, sj, num):
     q = deque()
-    q.append((i, j))
-    block = block
-    rainbow = []
-    cnt = 1
+    q.append((si, sj))
+    v = [[0] * N for _ in range(N)]
+    v[si][sj] = 1
+    same = [(si, sj)]  # 그룹에 포함된 블럭 좌표
+    rainbow_block = 0  # 무지개블럭 개수
+
     while q:
         ci, cj = q.popleft()
         for k in range(len(di)):
             ni = ci + di[k]
             nj = cj + dj[k]
-            if 0 <= ni < N and 0 <= nj < N and v[ni][nj] == 0:
-                if (arr[ni][nj] == block or arr[ni][nj] == 0):
-                    if arr[ni][nj] == 0:
-                        rainbow.append((ni,nj))
-                    v[ni][nj] = num
+            if (0 <= ni < N and 0 <= nj < N) and v[ni][nj] == 0:
+                if arr[ni][nj] == num:  # 같은 색 블록
                     q.append((ni, nj))
-                    cnt += 1
-    if cnt > 1:
-        dic[(i, j)] = [cnt]
-        if rainbow:
-            dic[(i, j)].append(len(rainbow))
-        else:
-            dic[(i, j)].append(0)
+                    v[ni][nj] = 1
+                    v_main[ni][nj] = 1
+                    same.append((ni, nj))
+                elif arr[ni][nj] == 0:  # 무지개 블럭
+                    q.append((ni, nj))
+                    v[ni][nj] = 1
+                    same.append((ni, nj))
+                    rainbow_block += 1
+    return (len(same), rainbow_block, same[0][0], same[0][1], same)
 
-    for ri,rj in rainbow:
-        v[ri][rj] = 0
 
+def gravity(arr):
+    for i in range(N - 2, -1, -1):
+        for j in range(N - 1, -1, -1):
+            if arr[i][j] < 0: continue
+            ci, cj = i, j
+            while 1:
+                ni = ci + 1
+                if 0 <= ni < N and arr[ni][cj] == -2:
+                    arr[ni][cj], arr[ci][cj] = arr[ci][cj], arr[ni][cj]
+                    ci = ni
+                else:
+                    break
+    return arr
 
-def shark(i,j,num):
-    q = deque()
-    v = [[0]*N for _ in range(N)]
-    q.append((i,j))
-    v[i][j] = 0
-    arr[i][j] = -2
-
-    while q:
-        ci,cj = q.popleft()
-        for k in range(len(di)):
-            ni = ci + di[k]
-            nj = cj + dj[k]
-            if 0 <= ni < N and 0 <= nj < N and v[ni][nj] == 0:
-                if (arr[ni][nj] == num or arr[ni][nj] == 0):
-                    arr[ni][nj]=-2
-                    q.append((ni,nj))
 
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-
 ans = 0
 
-def gravity(arr):
-    d = 2
-    for i in range(N - 1, -1, -1):
-        for j in range(N - 1, -1, -1):
-            si, sj = i, j
-            if arr[i][j] > -1:
-                while 1:
-                    ni = i + di[d]
-                    nj = j + dj[d]
-                    if (0 <= ni < N and 0 <= nj < N and arr[ni][nj] == -2):
-                        arr[ni][nj] = arr[i][j]
-                        arr[i][j] = -2
-                    else:
-                        i, j = si, sj
-                        break
-                    i, j = ni, nj
-    return arr
-
+# 블록그룹이 있는 동안
 while 1:
-    num = 1
-    dic = defaultdict(list)
-    v = [[0] * N for _ in range(N)]
+    # 1. 블럭 집합 찾기
+    groups = []
+    v_main = [[0] * N for _ in range(N)]
     for i in range(N):
         for j in range(N):
-            if v[i][j] == 0 and arr[i][j] > 0:
-                v[i][j] = num
-                bfs(i, j, arr[i][j], num)
-                num += 1
-    lst = sorted([(v, k) for k, v in dic.items()], key=lambda x: (-x[0][0], -x[0][1], -x[1][0], -x[1][1]))
-    if not lst: break
-    for point, (si, sj) in lst:
-        if arr[si][sj] > 0:
-            break
-        else:
-            continue
-    if point[0] == 1:
-        break
-    ans += (point[0]) ** 2
-    shark(si,sj,arr[si][sj])
+            if arr[i][j] > 0 and v_main[i][j] == 0:
+                res = bfs(i, j, arr[i][j])
+                # 집합개수, 무지개블럭 개수, 기준블럭좌표, 블럭좌표
+                if res[0] < 2: continue
+                groups.append(res)
 
+    # 블럭집합 없으면 끝
+    if not groups: break
+
+    # 가장 큰 블럭
+    groups.sort(reverse=True)
+    cnt, rainbow, std_i, std_j, blocks = groups[0]
+    ans += cnt ** 2
+    for bi, bj in blocks:
+        arr[bi][bj] = -2
+
+    # 3. 중력1
     arr = gravity(arr)
+
+    # 4. 격자 반시계 회전
     arr = [list(lst) for lst in zip(*arr)][::-1]
+
+    # 5. 중력2
     arr = gravity(arr)
+
 print(ans)
