89737428b46f0ea7b94139914bd9546d35b52421
Moon Sangdon <96267539+ddodon@users.noreply.github.com>
2024-04-03 20:39:50 +0900

    [Gold II] Title: 스타트 택시, Time: 284 ms, Memory: 115384 KB -BaekjoonHub

--- 백준/Gold/19238. 스타트 택시/스타트 택시.py
+++ 백준/Gold/19238. 스타트 택시/스타트 택시.py
@@ -1,12 +1,3 @@
-# 총 시간: 분
-# 문제 읽기: 5분
-# 풀이 구상: 10분
-# 로직 구현: ~3시 5분 (TC안맞음)
-    # 8분: 손님까지의 거리와 손님번호 변수를 순서를 바꿔서 리턴했었음
-# 제출 결과: (/2)
-# 오류 부분: TypeError (?)
-# 오류 수정: 손님을 태우고 목적지까지 갈 수 없는 경우 None 리턴했었음 (다 벽이거나)
-
 from collections import deque, defaultdict
 
 di = [-1, 0, 1, 0]
@@ -14,39 +5,37 @@ dj = [0, 1, 0, -1]
 
 
 def bfs(si, sj):
-    # global - 남은 손님 수 (가지치기 용)
     q = deque()
-    v = [[0] * N for _ in range(N)]
     q.append((si, sj))
+    v = [[0] * N for _ in range(N)]
     v[si][sj] = 1
-    customers = []
+    customer = []
     while q:
         ci, cj = q.popleft()
-        if p_arr[ci][cj] != 0:
-            customers.append((v[ci][cj] - 1, ci, cj, p_arr[ci][cj]))
+        if passenger[ci][cj] > 0:
+            customer.append((v[ci][cj] - 1, ci, cj, passenger[ci][cj]))
         for k in range(len(di)):
             ni = ci + di[k]
             nj = cj + dj[k]
             if 0 <= ni < N and 0 <= nj < N and v[ni][nj] == 0 and arr[ni][nj] != 1:
                 q.append((ni, nj))
                 v[ni][nj] = v[ci][cj] + 1
-    if customers:
-        customers.sort()
-        return customers[0]
-    else:
-        return 0
+    if customer:
+        customer.sort()
+        return customer[0]
+    return 0
 
 
-def drive(si, sj):
+def drive(si, sj, num):
     q = deque()
-    v = [[0] * N for _ in range(N)]
     q.append((si, sj))
+    v = [[0] * N for _ in range(N)]
     v[si][sj] = 1
-    (ei, ej) = departure[pi, pj]
+    (ei, ej) = depart[num]
     while q:
         ci, cj = q.popleft()
         if (ci, cj) == (ei, ej):
-            return (ei, ej, v[ci][cj] - 1)
+            return (v[ci][cj] - 1, ci, cj)
         for k in range(len(di)):
             ni = ci + di[k]
             nj = cj + dj[k]
@@ -58,45 +47,46 @@ def drive(si, sj):
 
 N, M, fuel = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-p_arr = [[0] * N for _ in range(N)]
+passenger = [[0] * N for _ in range(N)]
 si, sj = map(int, input().split())
 si, sj = si - 1, sj - 1
-passengers = []
-departure = defaultdict(tuple)
-for i in range(1, M + 1):
+depart = defaultdict(tuple)
+for m in range(1, M + 1):
     pi, pj, ei, ej = map(int, input().split())
     pi, pj, ei, ej = pi - 1, pj - 1, ei - 1, ej - 1
-    p_arr[pi][pj] = i
-    departure[(pi, pj)] = (ei, ej)
+    passenger[pi][pj] = m
+    depart[m] = (ei, ej)
 
 for _ in range(M):
-    # 손님 고르기
-    customer = bfs(si, sj)
-    if not customer:
+    # 1. 승객 고르기
+    res = bfs(si, sj)
+    # 1-1. 손님 태우러 못가는 경우
+    if not res:
         print(-1)
         break
-
-    p_dist, pi, pj, p_num = customer
-    if fuel - p_dist < 0:
+    dist, pi, pj, num = res
+    # 1-2. 연료 감소, 연료 확인
+    fuel -= dist
+    if fuel < 0:
         print(-1)
         break
-    fuel -= p_dist
-
-    # 운전
-    p_arr[pi][pj] = 0
     si, sj = pi, pj
-    arrived = drive(si, sj)
-    if not arrived:
+    passenger[pi][pj] = 0
+
+    # 2. 손님 이송
+    eres = drive(si, sj, num)
+    if not eres:
         print(-1)
         break
-    ei, ej, e_dist = arrived
-    if fuel - e_dist < 0:
+    dist, ei, ej = eres
+    # 연로 감소, 연료 확인
+    fuel -= dist
+    if fuel < 0:
         print(-1)
         break
-    fuel -= e_dist
     si, sj = ei, ej
 
-    # 연료 충언
-    fuel += (e_dist) * 2
+    # 3. 연료 충전
+    fuel += dist * 2
 else:
     print(fuel)
