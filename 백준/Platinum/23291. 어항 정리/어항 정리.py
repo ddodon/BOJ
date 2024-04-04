7b11641c5cadd51014b5f002f4c5a2ba0a84f8a9
Moon Sangdon <96267539+ddodon@users.noreply.github.com>
2024-04-04 14:56:32 +0900

    [Platinum V] Title: 어항 정리, Time: 148 ms, Memory: 111656 KB -BaekjoonHub

--- 백준/Platinum/23291. 어항 정리/어항 정리.py
+++ 백준/Platinum/23291. 어항 정리/어항 정리.py
@@ -1,26 +1,8 @@
-'''
-Memo Boj 
-메모리:
-시간:
-결과:  (/3)
-풀이시간: 
-▷ 문제 읽기: 12분
-▷ 풀이 구상: 분
-▷ 로직 구현: 
-    - 단계별로 나눠서 구현 (1~3단계부터 구상 후 구현) (슬라이싱 전치행렬)
-▷ 오류 수정: 
-    - 100분: 마지막 TC 1 안맞음(ans-> 13, 나->12)
-    - 180분: 도저히 못찾겠음
-    - 206분: 오류 발견 - 온도 조절부분 좌표 참조
-    - 210분: indexerror 디버깅하면서 이리 저리 손대다가 범위 실수 (regulate2 함수)
-▷ 오류 부분: 
-'''
-
+# stack 부분만 바꿔봄
 di = [-1, 0, 1, 0]
 dj = [0, 1, 0, -1]
 
-
-def stack_tank(lst):
+def stack(lst):
     idx = 1
     arr = []
     arr.append(lst[:idx])
@@ -36,87 +18,78 @@ def stack_tank(lst):
     return arr
 
 
-def regulate(arr):
-    nrr = [[0] * len(arr[-1]) for _ in range(len(arr))]
+def adjust(arr):
+    nrr = [lst[::] for lst in arr]
+    been = set()
     for i in range(len(arr)):
         for j in range(len(arr[i])):
-            nrr[i][j] += arr[i][j]
-    nnrr = [x[:] for x in nrr]
-
-    for i in range(len(arr)):
-        for j in range(len(arr[i])):
-            if not arr[i][j]: continue
             ci, cj = i, j
             for k in range(len(di)):
                 ni = ci + di[k]
                 nj = cj + dj[k]
-                if 0 <= ni < len(arr) and 0 <= nj < len(arr[i]) and nnrr[ci][cj] > nnrr[ni][nj] and nrr[ni][nj]:
-                    d = (arr[ci][cj] - arr[ni][nj]) // 5
-                    nrr[ci][cj] -= d
-                    nrr[ni][nj] += d
-    return nrr
+                if 0 <= ni < len(arr) and 0 <= nj < len(arr[ni]):
+                    d = abs(arr[ci][cj] - arr[ni][nj]) // 5
+                    if d < 1: continue
+                    if (ci, cj, ni, nj) in been: continue
+                    been.add((ci, cj, ni, nj))
+                    been.add((ni, nj, ci, cj))
+                    if arr[ci][cj] > arr[ni][nj]:
+                        nrr[ci][cj] -= d
+                        nrr[ni][nj] += d
+                    elif arr[ci][cj] < arr[ni][nj]:
+                        nrr[ci][cj] += d
+                        nrr[ni][nj] -= d
 
-def regulate2(arr):
-    nrr = [x[:] for x in arr]
-    for i in range(len(arr)):
-        for j in range(len(arr[0])):
-            ci, cj = i, j
-            for k in range(len(di)):
-                ni = ci + di[k]
-                nj = cj + dj[k]
-                if 0 <= ni < len(arr) and 0 <= nj <len(arr[0]) and arr[ci][cj] > arr[ni][nj]:
-                    d = (arr[ci][cj] - arr[ni][nj]) // 5
-                    nrr[ci][cj] -= d
-                    nrr[ni][nj] += d
-    return nrr
-
-def line(arr):
-    arr = [list(x) for x in zip(*arr[::-1])]
-    lst = []
-    for i in range(len(arr)):
-        for j in range(len(arr[0])):
-            if arr[i][j]:
-                lst.append(arr[i][j])
-    return lst
+    new_list = []
+    for j in range(len(nrr[-1])):
+        for i in range(len(nrr)-1,-1,-1):
+            if j >= len(nrr[i]):continue
+            new_list.append(nrr[i][j])
+    return new_list
 
 
-def stack_tank2(lst):
-    m = len(lst) // 2
-    up = lst[:m][::-1]
-    down = lst[m:]
-
+def stack2(lst):
     arr = []
-    arr.append(up[:m // 2])
-    up = up[m // 2:]
-    arr.append(down[:m // 2])
-    down = down[m // 2:]
-    arr = [list(x) for x in zip(*arr[::-1])]
-    arr = [list(x) for x in zip(*arr[::-1])]
-    arr.append(up)
-    arr.append(down)
+    l = len(lst) // 2
+    up = lst[:l][::-1]
+    down = lst[l:]
+
+    arr.append(up[:l // 2])
+    arr.append(down[:l // 2])
+    arr = [list(lst) for lst in zip(*arr[::-1])]
+    arr = [list(lst) for lst in zip(*arr[::-1])]
+    arr.append(up[l // 2:])
+    arr.append(down[l // 2:])
     return arr
 
 
 N, K = map(int, input().split())
-fishtank = list(map(int, input().split()))
+lst = list(map(int, input().split()))
+ans = 0  # 어항 정리 횟수
 
-ans = 0
+# 어항 정리 시작
 while 1:
-    mn = min(fishtank)
-    mx = max(fishtank)
-
+    mx = max(lst)
+    mn = min(lst)
     if mx - mn <= K:
         break
 
-    for i in range(len(fishtank)):
-        if fishtank[i] == mn: fishtank[i] += 1
+    # 1. 물고기 추가
+    for i in range(len(lst)):
+        if lst[i] == mn:
+            lst[i] += 1
 
-    arr = stack_tank(fishtank)
-    arr = regulate(arr)
-    fishtank = line(arr)
-    arr = stack_tank2(fishtank)
-    arr = regulate2(arr)
-    fishtank = line(arr)
-    # print2(arr)
+    # 2. 어항 쌓기
+    arr = stack(lst)
+
+    # 3. 물고기 조절 + 일렬 정리
+    lst = adjust(arr)
+
+    # 4. 어항 쌓기2
+    arr = stack2(lst)
+
+    # 5. 물고기 조절 + 일렬 정리
+    lst = adjust(arr)
+
     ans += 1
 print(ans)
