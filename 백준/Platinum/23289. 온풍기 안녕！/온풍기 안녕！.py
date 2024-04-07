7cd42ed64981320b6e745bb0cca036c98c061ac7
Moon Sangdon <96267539+ddodon@users.noreply.github.com>
2024-04-08 00:15:00 +0900

    [Platinum V] Title: 온풍기 안녕!, Time: 556 ms, Memory: 119636 KB -BaekjoonHub

--- 백준/Platinum/23289. 온풍기 안녕！/온풍기 안녕！.py
+++ 백준/Platinum/23289. 온풍기 안녕！/온풍기 안녕！.py
@@ -1,136 +1,165 @@
-# 총 시간: 분
-# 문제 읽기: 30분 ㄷㄷ
-# 풀이 구상: 분
-#   - 벽의 존재를 어떻게 구현할 것인가 고민 (멤버십 연산은 시간초과 우려) (2개 조합 최대 800개 연산을 매번)
-#   - 벽만 표시해둔 V배열?
-#   - 바람 전달 (BFS) -> 벽 만났을 때 방향별 처리 막힘
-#   - 녹화시간 2시간 47분: 벽배열로 벽 판단 시 로직상 오류  (벽이 한 칸 떨어져 존재하는 경우 3칸 중 2칸은 벽이 없는데 있다고 판단)
-#   - 벽을 set으로 두고 in 사용으로 전환
-#   - type별로 ni nj 방향 다르게 설정하니 성공 (종료 10분전 ㅠ)
-#   - 열 이동(move) 동시에 처리 -> 생각 안남 [배열 하나 더 만들어서] [빼줄 값, 더할 값 저장해두고] [합치기]
-# 로직 구현: 분
-# 제출 결과: 제한 시간 초과 (벽 처리에서 오래 걸림)
-# 오류 부분:
-# 오류 수정:
-# 기타: 풀이시작 전 6분간 화장실
-
 from collections import deque
 
-# 방향 → ← ↑ ↓
 di = [0, 0, -1, 1]
 dj = [1, -1, 0, 0]
+dir = [(-1, 1), (0, 1), (1, 1)]
+xdi = [-1, 0, 1]
+xdj = [1, 1, 1]
+opp = {}
 
-# 바람 방향
-hi = [[-1, 0, 1], [-1, 0, 1], [-1, -1, -1], [1, 1, 1]]
-hj = [[1, 1, 1], [-1, -1, -1], [-1, 0, 1], [-1, 0, 1]]
 
-
-def bfs(type, si, sj):
-    heat = 5
+def bfs(si, sj, d):
     q = deque()
-    q.append((si, sj, heat))
-    v = [[0] * C for _ in range(R)]
-    v[si][sj] = heat
+    q.append((si, sj))
+    v = [[0]*C for _ in range(R)]
+    v[si][sj] = 5
     while q:
-        ci, cj, ch = q.popleft()
-        for k in range(len(hi[0])):
-            ni = ci + hi[type - 1][k]
-            nj = cj + hj[type - 1][k]
-            if 0 <= ni < R and 0 <= nj < C and v[ni][nj] == 0:
-                if ch - 1 > 0:
-                    if type == 1 or type == 2:
-                        if k == 0 and (((ci, cj), (ni, cj)) in wall or ((ni, cj), (ni, nj)) in wall): continue
-                        if k == 1 and (((ci, cj), (ni, nj)) in wall): continue
-                        if k == 2 and (((ci, cj), (ni, cj)) in wall or ((ni, cj), (ni, nj)) in wall): continue
-                    elif type == 3 or type == 4:
-                        if k == 0 and (((ci, cj), (ci, nj)) in wall or ((ci, nj), (ni, nj)) in wall): continue
-                        if k == 1 and (((ci, cj), (ni, nj)) in wall): continue
-                        if k == 2 and (((ci, cj), (ci, nj)) in wall or ((ci, nj), (ni, nj)) in wall): continue
-                    v[ni][nj] = ch - 1
-                    q.append((ni, nj, ch - 1))
+        ci, cj = q.popleft()
+        if v[ci][cj] == 0:
+            break
+        for k in range(3):
+            if d == 0:
+                ni = ci + xdi[k]
+                nj = cj + xdj[k]
+                if 0 <= ni < R and 0 <= nj < C and v[ni][nj] == 0:
+                    if k == 0:  # 대각선 위
+                        if ((ci, cj), (ni, cj)) in walls or ((ni, cj), (ni, nj)) in walls: continue
+                    elif k == 1:  # 직선
+                        if ((ci, cj), (ni, nj)) in walls: continue
+                    else:  # 대각선 아래
+                        if ((ci, cj), (ni, cj)) in walls or ((ni, cj), (ni, nj)) in walls: continue
+                    v[ni][nj] = v[ci][cj] - 1
+                    q.append((ni, nj))
+
+            elif d == 1:
+                ni = ci + xdi[k]
+                nj = cj - xdj[k]
+                if 0 <= ni < R and 0 <= nj < C and v[ni][nj] == 0:
+                    if k == 0:  # 대각선 위
+                        if ((ci, cj), (ni, cj)) in walls or ((ni, cj), (ni, nj)) in walls: continue
+                    elif k == 1:  # 직선
+                        if ((ci, cj), (ni, nj)) in walls: continue
+                    else:  # 대각선 아래
+                        if ((ci, cj), (ni, cj)) in walls or ((ni, cj), (ni, nj)) in walls: continue
+                    v[ni][nj] = v[ci][cj] - 1
+                    q.append((ni, nj))
+            elif d == 2:
+                ni = ci - xdj[k]
+                nj = cj + xdi[k]
+                if 0 <= ni < R and 0 <= nj < C and v[ni][nj] == 0:
+                    if k == 0:  # 대각선 위
+                        if ((ci, cj), (ci, nj)) in walls or ((ci, nj), (ni, nj)) in walls: continue
+                    elif k == 1:  # 직선
+                        if ((ci, cj), (ni, nj)) in walls: continue
+                    else:  # 대각선 아래
+                        if ((ci, cj), (ci, nj)) in walls or ((ci, nj), (ni, nj)) in walls: continue
+                    v[ni][nj] = v[ci][cj] - 1
+                    q.append((ni, nj))
+            else:
+                ni = ci + xdj[k]
+                nj = cj - xdi[k]
+                if 0 <= ni < R and 0 <= nj < C and v[ni][nj] == 0:
+                    if k == 0:  # 대각선 위
+                        if ((ci, cj), (ci, nj)) in walls or ((ci, nj), (ni, nj)) in walls: continue
+                    elif k == 1:  # 직선
+                        if ((ci, cj), (ni, nj)) in walls: continue
+                    else:  # 대각선 아래
+                        if ((ci, cj), (ci, nj)) in walls or ((ci, nj), (ni, nj)) in walls: continue
+                    v[ni][nj] = v[ci][cj] - 1
+                    q.append((ni, nj))
     return v
 
 
-def heating(home):
-    for type, i, j in heater:
-        si = i + di[type - 1]
-        sj = j + dj[type - 1]
-        tmp = bfs(type, si, sj)
-        for r in range(R):
-            for c in range(C):
-                home[r][c] += tmp[r][c]
-    return home
+def heating(temp):
+    for d, ci, cj in heater:
+        ci = ci + di[d - 1]
+        cj = cj + dj[d - 1]
+        res = bfs(ci, cj, d - 1)
+        for i in range(R):
+            for j in range(C):
+                temp[i][j]+=res[i][j]
+    return temp
 
-
-def move(home):
-    new_home = [[0] * C for _ in range(R)]
-
+def adjust(arr):
+    nrr = [lst[::] for lst in arr]
     for i in range(R):
         for j in range(C):
-            if home[i][j] != 0:
-                ci, cj = i, j
+            if arr[i][j]>0:
+                ci,cj=i,j
                 for k in range(len(di)):
-                    ni = ci + di[k]
-                    nj = cj + dj[k]
-                    if 0 <= ni < R and 0 <= nj < C and (home[i][j] > home[ni][nj]):
-                        if ((i, j), (ni, nj)) in wall: continue
-                        new_home[i][j] -= (home[i][j] - home[ni][nj]) // 4
-                        new_home[ni][nj] += (home[i][j] - home[ni][nj]) // 4
+                    ni = ci+di[k]
+                    nj = cj+dj[k]
+                    if 0<=ni<R and 0<=nj<C and arr[ci][cj]>arr[ni][nj]:
+                        if ((ci,cj),(ni,nj)) in walls: continue
+                        diff = (arr[ci][cj]-arr[ni][nj])//4
+                        nrr[ci][cj] -= diff
+                        nrr[ni][nj] += diff
+    return nrr
+
+def side(arr):
     for i in range(R):
-        for j in range(C):
-            home[i][j] += new_home[i][j]
-    return home
+        if arr[i][0]>0:
+            arr[i][0]-=1
+        if arr[i][C-1]>0:
+            arr[i][C-1]-=1
+
+    for j in range(1,C-1):
+        if arr[0][j]>0:
+            arr[0][j]-=1
+        if arr[-1][j]>0:
+            arr[-1][j]-=1
+    return arr
+
+def check():
+    for ci,cj in to_check:
+        if temp[ci][cj]<K:
+            return False
+    return True
 
 
-def outline(home):
-    for i in range(R):
-        if home[i][0] > 0:
-            home[i][0] -= 1
-        if home[i][C - 1] > 0:
-            home[i][C - 1] -= 1
-
-    for i in range(1, C - 1):
-        if home[0][i] > 0:
-            home[0][i] -= 1
-        if home[R - 1][i] > 0:
-            home[R - 1][i] -= 1
-    return home
 
 
 R, C, K = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(R)]
-home = [[0] * C for _ in range(R)]  # 온도를 나타낼 배열 (home)
-check = []  # 조사해야 하는 칸 (r,c)
-heater = []  # 온풍기 좌표 (종류,r,c)
+temp = [[0] * C for _ in range(R)]
+to_check = []  # 조사칸
+heater = []  # 온풍기
 for i in range(R):
     for j in range(C):
-        if arr[i][j] == 0: continue
         if arr[i][j] == 5:
-            check.append((i, j))
-        elif arr[i][j] < 5:
+            to_check.append((i, j))
+        elif 0 < arr[i][j] < 5:
             heater.append((arr[i][j], i, j))
 W = int(input())
-wall = set()  # 벽이 있는 부분
+walls = set()
 for _ in range(W):
     x, y, t = map(int, input().split())
     x, y = x - 1, y - 1
     if t == 0:
-        wall.add(((x, y), (x - 1, y)))
-        wall.add(((x - 1, y), (x, y)))
+        walls.add(((x, y), (x - 1, y)))
+        walls.add(((x - 1, y), (x, y)))
     else:
-        wall.add(((x, y), (x, y + 1)))
-        wall.add(((x, y + 1), (x, y)))
-choco = 0  # 초콜릿
-while 1:
-    if choco > 100: break
-    home = heating(home)
-    home = move(home)
-    home = outline(home)
+        walls.add(((x, y), (x, y + 1)))
+        walls.add(((x, y + 1), (x, y)))
+
+choco = 0
+
+for _ in range(100):
+    # 1. 온풍기에서 바람
+    temp = heating(temp)
+
+    # 2. 온도 조절
+    temp = adjust(temp)
+
+    # 3. 테두리 -1
+    temp = side(temp)
+
+    # 4. 초코 +1
     choco += 1
-    cnt = 0
-    for ci, cj in check:
-        if home[ci][cj] >= K:
-            cnt += 1
-    if cnt >= len(check):
+
+    # 5. 조사
+    if check():
+        print(choco)
         break
-print(choco)
+else:
+    print(101)
