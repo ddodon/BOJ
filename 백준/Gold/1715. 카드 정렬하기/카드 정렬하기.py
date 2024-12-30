346b993b4dcc1b82e4371effb5e161df3603c22c
Moon Sangdon <96267539+ddodon@users.noreply.github.com>
2024-12-30 21:17:24 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 220 ms, Memory: 113040 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -1,17 +1,18 @@
 import sys
 import heapq
 
-input = sys.stdin.readline
-N = int(input())
-hq = []
-res = 0
-for i in range(N):
-    x = int(input())
-    heapq.heappush(hq,x)
+def solution():
+    N = int(input())
+    hq = [int(input()) for _ in range(N)]
+    heapq.heapify(hq)
+    res = 0
+    for _ in range(N - 1):
+        a = heapq.heappop(hq)
+        b = heapq.heappop(hq)
+        heapq.heappush(hq, a + b)
+        res += (a + b)
+    print(res)
 
-for i in range(N-1):
-    a = heapq.heappop(hq)
-    b = heapq.heappop(hq)
-    heapq.heappush(hq,a+b)
-    res += (a+b)
-print(res)
+if __name__=="__main__":
+    input = sys.stdin.readline
+    solution()
